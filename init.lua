e57876863a72a89fcb9cafc1fe551865cacfd9ef
neczju <neczju@tuta.com>
2024-09-05 19:12:57 +0200

    updated colorscheme

--- init.lua
+++ init.lua
@@ -778,16 +778,18 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'neczju/alabaster.nvim',
+    'aktersnurra/no-clown-fiesta.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
-      -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'alabaster'
+      vim.cmd.colorscheme 'no-clown-fiesta'
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
       vim.cmd.hi 'Normal guibg=none'
+      vim.cmd.hi 'LineNr guibg=none'
+      vim.cmd.hi 'SignColumn guibg=none'
+      vim.cmd.hi 'EndOfBuffer guibg=none'
     end,
   },
 
