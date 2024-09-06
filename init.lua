1514ad030918659c2f6eaf1dd84a99e64641163c
neczju <neczju@tuta.com>
2024-09-06 21:32:55 +0200

    changed colorscheme to gruvbox

--- init.lua
+++ init.lua
@@ -773,23 +773,13 @@ require('lazy').setup({
     end,
   },
 
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'aktersnurra/no-clown-fiesta.nvim',
+  { -- colorscheme
+    'ellisonleao/gruvbox.nvim',
+    opts = { transparent_mode = true },
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'no-clown-fiesta'
-      -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
-      vim.cmd.hi 'Normal guibg=none'
-      vim.cmd.hi 'LineNr guibg=none'
-      vim.cmd.hi 'SignColumn guibg=none'
-      vim.cmd.hi 'EndOfBuffer guibg=none'
+      -- Load colorscheme
+      vim.cmd.colorscheme 'gruvbox'
     end,
   },
 
